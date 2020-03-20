55a2445b0f2fee93e31e1caa849fb32d23add3f9
AUGUSTO664 <1909032@upy.edu.mx>
2020-03-20 17:31:00 -0600

    it have noy added the extracredits yet

--- hard_way_series/exercise10.c
+++ hard_way_series/exercise10.c
@@ -1,1 +1,28 @@
 //this is the exercise 10
+//Arryas of strings, looping, here's code that will print out any command line arguments
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+
+ int i = 0;
+ //go trough each string in argv
+ //why am I skipping argv[0]?
+
+ for(i = 1; i < argc; i++){
+      printf("arg %d: %s\n",i, argv[i]);
+
+ }
+
+ //let's make our own array of strings
+ char *states[]= {"california", "oregon", "washington", "texas"};
+
+ int num_states = 4;
+ //this for prints all the array's elements
+ for(i=0; i<num_states; i++){
+    printf("state %d: %s\n", i, states[i]);
+ }
+
+ return 0;
+}
