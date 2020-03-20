daaea0ac7eb439bc2fa6361d1e2c6ba3f3e48a3b
AUGUSTO664 <1909032@upy.edu.mx>
2020-03-20 01:37:43 -0600

    in this commit I finished writing the code without comentaries

--- hard_way_series/exercise8.c
+++ hard_way_series/exercise8.c
@@ -21,8 +21,11 @@ int main(int argc, char *argv[])
   printf("The size of name (char[]): %ld\n", sizeof(name));
   printf("The number of chars: %ld\n", sizeof(name) / sizeof(char));
 
-  
+  printf("The size of full_name (char[]): %ld\n",sizeof(full_name));
+  printf("The number of chars: %ld\n",sizeof(full_name) / sizeof(char));
+  printf("name=\"%s\" and full_name=\"%s\"\n",name, full_name);
 
 
- return (0);
+  return (0);
+
 }
